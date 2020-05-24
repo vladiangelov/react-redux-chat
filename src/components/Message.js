e2a61bcad821da055a7a93203b847cb64176a16c
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-05-24 14:06:05 +0100

    Added formatted time, changed high level text align

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Message = (props) => {
+  let date = new Date(props.message.created_at);
   return (
-    <div>
-      <p>{props.message.author}{props.message.created_at}</p>
-      <div>{props.message.content}</div>
+    <div className="message">
+      <p><strong>{props.message.author}</strong> {date.toLocaleTimeString('en-US')}</p>
+      <p>{props.message.content}</p>
     </div>
   )
 }
